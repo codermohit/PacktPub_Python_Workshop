bcde217217bc927ab4470555a0bc1914a6ac3121
codermohit <iamthecodermohit@gmail.com>
2020-04-16 23:32:15 -0700

    Day Ten

---
+++ dynamic.py
@@ -0,0 +1,23 @@
+stored_results = {}
+def sum_to_n(n):
+    result = 0
+    for i in reversed(range(n)):
+        result += i + 1
+    stored_results[n] = result
+    print(result)
+
+stored_results = {}
+def sum_to_n(n):
+    result = 0
+    for i in reversed(range(n)):
+        if i + 1 in stored_results:
+            print('Stopping sum at %s because we have previously computed it' % str(i + 1))
+            result += stored_results[i + 1]
+            break
+        else:
+            result += i + 1
+    stored_results[n] = result
+    print(result)
+
+sum_to_n(5)
+sum_to_n(6)
